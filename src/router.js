a6efba1e0c8eafc988304e5ef73e5023ac7b8e58
anharfathoni <anharaf28@gmail.com>
2018-12-13 22:15:44 +0700

    question

--- src/router.js
+++ src/router.js
@@ -20,6 +20,22 @@ export default new Router({
       // this generates a separate chunk (about.[hash].js) for this route
       // which is lazy-loaded when the route is visited.
       component: () => import(/* webpackChunkName: "about" */ './views/About.vue')
+    },
+    {
+      path: '/admin',
+      name: 'admin',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "admin" */ './components/admin.vue')
+    },
+    {
+      path: '/question',
+      name: 'question',
+      // route level code-splitting
+      // this generates a separate chunk (about.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import(/* webpackChunkName: "question" */ './components/showQuestions.vue')
     }
   ]
 })
