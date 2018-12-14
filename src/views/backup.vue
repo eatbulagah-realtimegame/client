02a9e1994c37a83751b2129fa8010713058b0f79
muhamadabduh <muhamad.abduh.muh@gmail.com>
2018-12-14 14:56:07 +0700

    edit room vue

--- src/views/backup.vue
+++ src/views/backup.vue
@@ -1,6 +1,5 @@
 <template>
     <div>
-        <h2>{{isPlaying}}</h2>
         <div class="container">
             <div class="row">
                 <div class="col-md-6">
@@ -22,7 +21,7 @@
                             class="text-center">
                         <h4>Score {{myPoints}}</h4>
                         <form @submit.prevent="checkPrediction()">
-                            <div v-if="isPlaying && initialized" class="d-flex flex-column justify-content-center mb-2" >
+                            <div v-if="!gameOver && initialized" class="d-flex flex-column justify-content-center mb-2" >
                                 <label for="">Your Answer</label>
                                 <input type="text" v-model="myAnswer" class="form-control">
                             </div>
@@ -34,7 +33,7 @@
         </div>
         
         <h1>Score {{myPoints}}</h1>
-        <div v-if="isPlaying">
+        <div v-if="!gameOver">
             <div class="bg-dark text-white">
                 <p class="text-white">Input your answer : </p>
                 {{ currentPlayerName }}
@@ -52,21 +51,13 @@
                 </p>
             </div>
             <form @submit.prevent="checkPrediction()">
-                <div v-if="isPlaying && initialized" class="d-flex flex-column justify-content-center" >
+                <div v-if="!gameOver && initialized" class="d-flex flex-column justify-content-center" >
                     <label for="">Your Answer</label>
                     
                     <input type="text" v-model="myAnswer" class="form-control">
                     <button class="btn btn-primary" type="">Submit</button>
                 </div>
             </form>
-            <div>
-                {{keyQuestion}}
-                {{selectedQuestion.question}} <br>
-                {{selectedQuestion.answer}}
-            </div>
-        </div>
-        <div v-if="!isPlaying">
-            WINNER : {{winner}}
         </div>
     </div>
 </template>
@@ -81,7 +72,6 @@ export default {
             currentPlayerName: localStorage.getItem('username'),
             initialized: false,
             gameOver: false,
-            isPlaying: true,
             player1: '',
             player2: '',
             player1Info: '',
@@ -90,16 +80,8 @@ export default {
             room: '',
             userId: '',
             myAnswer: '',
-            selectedQuestion : {
-                question: '',
-                answer: ''
-            },
-            keyQuestion: '',
-            myPoints: 0,
-            winner: '',
-            listQuestions: [],
-            roomNow: localStorage.getItem('room'),
             realAnswer: 'ayams', //still hardcoded,
+            myPoints: 0,
             imgUrl: 'https://storage.googleapis.com/client-trekshop/tidakSRT.gif.gif',
             images: [{
                 name: 'bisajadi',
@@ -142,20 +124,18 @@ export default {
         checkPrediction() {
             let self = this
             let ref = database.ref('room/' + self.room + '/users/' + this.userId)
-            if(this.selectedQuestion.answer === this.myAnswer) {
+            if(this.realAnswer === this.myAnswer) {
                 let currentPoint = 0
                 ref.once('value', function(snapshot) {
                     let refPoint = database.ref('room/' + self.room + '/users/' + self.userId + '/points')
                     currentPoint = snapshot.val().points + 1
                     refPoint.set(currentPoint)
-                    self.myPoints = currentPoint
                     self.checkPoints(currentPoint)
-                    self.removeQuestion()
                 })
                 // alert('BENAR')
                 this.imgUrl = this.images[2].url
                 
-            } else if(this.selectedQuestion.answer.slice(0,4) === this.myAnswer.slice(0,4)) {
+            } else if(this.realAnswer.slice(0,4) === this.myAnswer.slice(0,4)) {
                 // alert('BISA JADI')
                 this.myAnswer = ''
                 this.imgUrl = this.images[0].url
@@ -165,72 +145,26 @@ export default {
                 this.imgUrl = this.images[1].url
             }
         },
-        removeQuestion() {
-            let self = this
-            return new Promise((resolve, reject) => {
-                database.ref('room/' + self.room + '/questions/' + self.keyQuestion).remove()
-                resolve()
-            })
-        },
         checkPoints(point) {
-            let self = this
             if(point >= 5) {
-                // this.winner = this.currentPlayerName
-                // this.gameOver = true
-                database.ref('room/' + self.room + '/isPlaying/status').set(false)
-                database.ref('room/' + self.room + '/winner').set(self.currentPlayerName)
-
+                this.myPoints = point
                 alert('GAME SELESAI')
             } else {
+                this.myPoints = point
                 console.log(point)
             }
         },
-        getQuestions(){
-            let self = this
-            self.listQuestions = []
-            return database.ref('room/' + self.roomNow + '/questions').once('value')
-            .then( snapshot => {
-                let dataQuestions = snapshot.val()
-                console.log("+++++====")
-                console.log(dataQuestions)
-                Object.keys(dataQuestions).forEach( key => {
-                    self.listQuestions.push({
-                        key: key,
-                        question: dataQuestions[key].question,
-                        answer: dataQuestions[key].answer
-                    })
-                })
-                
-                self.selectedQuestion = self.listQuestions[0]
-                self.keyQuestion = self.listQuestions[0].key
-                console.log('new KEY', self.keyQuestion)
-                console.log('new Quest', self.selectedQuestion)
-                // console.log('=======')
-                // console.log(self.listQuestions)
-                // self.randomQuestion()
-            })
-        },
-        // randomQuestion(){
-        //     this.selectedQuestion = this.listQuestions[Math.floor(Math.random()*4)]
-        //     console.log(this.selectedQuestion)
-        // }
+        // getQuestions(){
+        //     let self = this
+        //     return database.ref('questions').once('value')
+        //     .then( snapshot = {
+        //         let dataQuestions = snapshot.val()
+        //         Object.keys
+        //     })
+        // },
     },
     created() {
-        let self = this
         this.checkAllUsers()
-        this.getQuestions()
-        database.ref('room/' + self.roomNow + '/questions').on('value', function(snapshot) {
-            self.getQuestions()
-        })
-        database.ref('room/' + self.room + '/users/' + this.userId).on('value', function(snapshot) {
-            self.myPoints = snapshot.val().points
-        })
-        database.ref('room/' + self.room + '/isPlaying').on('value', function(snapshot) {
-            self.isPlaying = snapshot.val().status
-        })
-        database.ref('room/' + self.room + '/winner').on('value', function(snapshot) {
-            self.winner = snapshot.val()
-        })
     }
 }
 </script>
