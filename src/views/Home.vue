af349496e7f475733d971491f2691af55009278f
abedlubis <lubisabednego@gmail.com>
2018-12-14 14:48:39 +0700

    login done

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -42,7 +42,7 @@ export default {
     data() {
         return {
             room: '',
-            name: ''
+            name: '',
             // roomList: []
             listQuestions: [],
             selectedQuestion: {}
